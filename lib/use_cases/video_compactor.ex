aa0156590d19d516e35a9b547f30305243d79f5f
Diego G. lima <diego.lima@alice.com>
2025-06-24 21:12:41 -0300

    add client

--- lib/use_cases/video_compactor.ex
+++ lib/use_cases/video_compactor.ex
@@ -5,15 +5,20 @@ defmodule VideoCompactor.UseCases.VideoCompactor do
   @zip_path "/tmp/zip"
   @tmp_path "/tmp"
 
-  def run(%Video{} = video, repository) do
+  @status_compacted "COMPACTED"
+  @status_error "ERROR"
+
+  def run(%Video{} = video, repository, video_manager) do
     with charlist_path <- String.to_charlist("#{@tmp_path}/#{video.temp_file_path}"),
          {:ok, zip_path} <- :zip.create("#{@zip_path}/#{video.id}.zip", [charlist_path]),
-         {:ok, _} <- repository.create(%{video | zip_path: zip_path}) do
+         {:ok, _} <- repository.create(%{video | zip_path: zip_path}),
+         :ok <- video_manager.update_status(video, @status_compacted) do
       Logger.info("Zip path: #{inspect(zip_path)}")
       :ok
     else
       error ->
         Logger.error("Error compacting video: #{inspect(error)}")
+        video_manager.update_status(video, @status_error)
         {:error, error}
     end
   end
